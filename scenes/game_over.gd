7c19d2324d93774d8bd44c94aae614c0fb1a214f
DanteUrdaneta <danteurd@gmail.com>
2024-01-28 07:23:47 -0400

    game over la secuela

--- scenes/game_over.gd
+++ scenes/game_over.gd
@@ -4,7 +4,6 @@ extends Control
 @onready var retry_button = $VBoxContainer/seperador2/Button
 @onready var menu_button = $VBoxContainer/seperador2/Button2
 @onready var next_game =preload("res://scenes/game.tscn") as PackedScene
-@onready var next_room =preload("res://menu.tscn") as PackedScene
 
 func _ready():
 	retry_button.button_down.connect(on_start_pressed)
@@ -14,4 +13,4 @@ func on_start_pressed() -> void:
 	get_tree().change_scene_to_packed(next_game)
 	
 func on_exit_pressed() -> void:
-	get_tree().change_scene_to_packed(next_room)
+	get_tree().quit()
